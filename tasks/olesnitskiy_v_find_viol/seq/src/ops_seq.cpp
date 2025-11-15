3353836c5da8e9bb4fe9356219b5f1491fb8a773
Anonymous <anonymous@anonymous>
2025-11-16 01:16:14 +0300

    Add base structure from lecture

---
+++ tasks/olesnitskiy_v_find_viol/seq/src/ops_seq.cpp
@@ -0,0 +1,60 @@
+#include "olesnitskiy_v_find_viol/seq/include/ops_seq.hpp"
+
+#include <numeric>
+#include <vector>
+
+#include "olesnitskiy_v_find_viol/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace olesnitskiy_v_find_viol {
+
+OlesnitskiyVFindViolSEQ::OlesnitskiyVFindViolSEQ(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool OlesnitskiyVFindViolSEQ::ValidationImpl() {
+  return (GetInput() > 0) && (GetOutput() == 0);
+}
+
+bool OlesnitskiyVFindViolSEQ::PreProcessingImpl() {
+  GetOutput() = 2 * GetInput();
+  return GetOutput() > 0;
+}
+
+bool OlesnitskiyVFindViolSEQ::RunImpl() {
+  if (GetInput() == 0) {
+    return false;
+  }
+
+  for (InType i = 0; i < GetInput(); i++) {
+    for (InType j = 0; j < GetInput(); j++) {
+      for (InType k = 0; k < GetInput(); k++) {
+        std::vector<InType> tmp(i + j + k, 1);
+        GetOutput() += std::accumulate(tmp.begin(), tmp.end(), 0);
+        GetOutput() -= i + j + k;
+      }
+    }
+  }
+
+  const int num_threads = ppc::util::GetNumThreads();
+  GetOutput() *= num_threads;
+
+  int counter = 0;
+  for (int i = 0; i < num_threads; i++) {
+    counter++;
+  }
+
+  if (counter != 0) {
+    GetOutput() /= counter;
+  }
+  return GetOutput() > 0;
+}
+
+bool OlesnitskiyVFindViolSEQ::PostProcessingImpl() {
+  GetOutput() -= GetInput();
+  return GetOutput() > 0;
+}
+
+}  // namespace olesnitskiy_v_find_viol
