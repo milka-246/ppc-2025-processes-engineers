85ba658641fc3a95269cbd7b4e120216de73e334
Anonymous <anonymous@anonymous>
2025-11-20 06:58:17 +0300

    fix_some_bag

--- tasks/olesnitskiy_v_find_viol/tests/functional/main.cpp
+++ tasks/olesnitskiy_v_find_viol/tests/functional/main.cpp
@@ -59,7 +59,7 @@ TEST_P(OlesnitskiyVFindViolFuncTests, FindViolations) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 8> kTestParam = {
+const std::array<TestType, 10> kTestParam = {
     std::make_tuple(std::vector<double>{}, 0, "empty"),
     std::make_tuple(std::vector<double>{1.0}, 0, "single"),
     std::make_tuple(std::vector<double>{1.0, 2.0, 3.0, 4.0, 5.0}, 0, "sorted_asc"),
@@ -67,7 +67,9 @@ const std::array<TestType, 8> kTestParam = {
     std::make_tuple(std::vector<double>{1.0, 3.0, 2.0, 5.0, 4.0}, 2, "mixed"),
     std::make_tuple(std::vector<double>{1.0, 2.0, 2.0, 3.0, 3.0}, 0, "duplicates"),
     std::make_tuple(std::vector<double>{1.0, 1.0 + 1e-11, 1.0 + 1e-9}, 0, "precision_low"),
-    std::make_tuple(std::vector<double>{1.0, 1.0 - 1e-8, 1.0 - 2e-8}, 2, "precision_high")};
+    std::make_tuple(std::vector<double>{1.0, 1.0 - 1e-8, 1.0 - 2e-8}, 2, "precision_high"),
+    std::make_tuple(std::vector<double>{1.0, 2.0}, 1, "two_numbers"),
+    std::make_tuple(std::vector<double>{3.0, 1.0, 2.0}, 1, "three_numbers")};
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<OlesnitskiyVFindViolMPI, InType>(kTestParam, PPC_SETTINGS_olesnitskiy_v_find_viol),
