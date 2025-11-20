a24416fa935c2c7007415cf323e0cbbbf8197954
Anonymous <anonymous@anonymous>
2025-11-20 03:28:48 +0300

    Add report

--- tasks/olesnitskiy_v_find_viol/mpi/src/ops_mpi.cpp
+++ tasks/olesnitskiy_v_find_viol/mpi/src/ops_mpi.cpp
@@ -29,12 +29,12 @@ bool OlesnitskiyVFindViolMPI::RunImpl() {
     GetOutput() = 0;
     return true;
   }
+  const auto &input_data = GetInput();
+  int total_size = input_data.size();
+  const double epsilon = 1e-10;
   int world_size, world_rank;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  const auto &input_data = GetInput();
-  int total_size = static_cast<int>(input_data.size());
-
   if (total_size <= world_size) {
     int viol = 0;
     if (world_rank == 0) {
@@ -49,53 +49,24 @@ bool OlesnitskiyVFindViolMPI::RunImpl() {
     GetOutput() = viol;
     return true;
   }
-  std::vector<int> sendcounts(world_size, 0);
-  std::vector<int> displs(world_size, 0);
-  std::vector<double> boundary_elements_for_scatter(world_size, 0.0);
-  if (world_rank == 0) {
-    int base_chunk = total_size / world_size;
-    int remainder = total_size % world_size;
-    int current_displ = 0;
-
-    for (int i = 0; i < world_size; i++) {
-      sendcounts[i] = base_chunk + (i < remainder ? 1 : 0);
-      displs[i] = current_displ;
-
-      if (i == 0) {
-        boundary_elements_for_scatter[i] = 0.0;
-      } else {
-        int prev_process_last_idx = displs[i - 1] + sendcounts[i - 1] - 1;
-        boundary_elements_for_scatter[i] = input_data[prev_process_last_idx];
-      }
-
-      current_displ += sendcounts[i];
-    }
-  }
-  int my_chunk_size = 0;
-  MPI_Scatter(sendcounts.data(), 1, MPI_INT, &my_chunk_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  std::vector<double> local_data(my_chunk_size);
-  MPI_Scatterv(input_data.data(), sendcounts.data(), displs.data(), MPI_DOUBLE, local_data.data(), my_chunk_size,
-               MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  double prev_element;
-  MPI_Scatter(boundary_elements_for_scatter.data(), 1, MPI_DOUBLE, &prev_element, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  int base_chunk = total_size / world_size;
+  int remainder = total_size % world_size;
+  int my_start = world_rank * base_chunk + std::min(world_rank, remainder);
+  int my_end = my_start + base_chunk + (world_rank < remainder ? 1 : 0);
   int local_viol = 0;
-  const double epsilon = 1e-10;
-  if (world_rank > 0 && my_chunk_size > 0) {
-    if (prev_element - local_data[0] > epsilon) {
+  for (int i = my_start; i < my_end - 1; i++) {
+    if (input_data[i] - input_data[i + 1] > epsilon) {
       local_viol++;
     }
   }
-  if (my_chunk_size > 1) {
-    for (int i = 0; i < my_chunk_size - 1; i++) {
-      if (local_data[i] - local_data[i + 1] > epsilon) {
-        local_viol++;
-      }
+  if (world_rank > 0) {
+    if (input_data[my_start - 1] - input_data[my_start] > epsilon) {
+      local_viol++;
     }
   }
   int total_viol;
   MPI_Allreduce(&local_viol, &total_viol, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
   GetOutput() = total_viol;
-
   return true;
 }
 
